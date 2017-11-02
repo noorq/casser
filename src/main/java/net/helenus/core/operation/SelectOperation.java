864c4af5af8aedb9ef25c11c2b4146ac59cc9f4c
Greg Burd <gburd@onshape.com>
2017-11-02 16:09:47 -0400

    We only transition to allow filtering and only if we've not already set it.

--- src/main/java/net/helenus/core/operation/SelectOperation.java
+++ src/main/java/net/helenus/core/operation/SelectOperation.java
@@ -291,21 +291,23 @@ public final class SelectOperation<E> extends AbstractFilterStreamOperation<E, S
 				where.and(filter.getClause(sessionOps.getValuePreparer()));
 				HelenusProperty prop = filter.getNode().getProperty();
 				boolean isFirstIndex = true;
-				switch (prop.getColumnType()) {
-					case PARTITION_KEY :
-					case CLUSTERING_COLUMN :
-						break;
-					default :
-						// When using non-Cassandra-standard 2i types or when using more than one
-						// indexed column or non-indexed columns the query must include ALLOW FILTERING.
-						if (prop.caseSensitiveIndex()) {
-							allowFiltering = true;
-						} else if (prop.getIndexName() != null) {
-							allowFiltering |= !isFirstIndex;
-							isFirstIndex = false;
-						} else {
-							allowFiltering = true;
-						}
+				if (allowFiltering == false) {
+					switch (prop.getColumnType()) {
+						case PARTITION_KEY :
+						case CLUSTERING_COLUMN :
+							break;
+						default :
+							// When using non-Cassandra-standard 2i types or when using more than one
+							// indexed column or non-indexed columns the query must include ALLOW FILTERING.
+							if (prop.caseSensitiveIndex()) {
+								allowFiltering = true;
+							} else if (prop.getIndexName() != null) {
+								allowFiltering |= !isFirstIndex;
+								isFirstIndex = false;
+							} else {
+								allowFiltering = true;
+							}
+					}
 				}
 			}
 		}
